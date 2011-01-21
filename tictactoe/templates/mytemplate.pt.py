6db16010a8afa0e38a6c022867bdfaee49959373
Ignas Mikalajūnas <ignas.mikalajunas@gmail.com>
2011-01-21 20:26:06 +0200

    VEIKIA 2

--- tictactoe/templates/mytemplate.pt.py
+++ tictactoe/templates/mytemplate.pt.py
@@ -1,24 +1,25 @@
 registry = dict(version=0)
 def bind():
     from cPickle import loads as _loads
-    _attrs_181905260 = _loads('(dp1\n.')
     _lookup_attr = _loads('cchameleon.core.codegen\nlookup_attr\np1\n.')
     _init_scope = _loads('cchameleon.core.utils\necontext\np1\n.')
-    _attrs_181903468 = _loads('(dp1\n.')
-    _attrs_181882732 = _loads('(dp1\n.')
-    _attrs_181904172 = _loads('(dp1\n.')
-    _attrs_181903980 = _loads('(dp1\n.')
-    _attrs_181904428 = _loads('(dp1\n.')
-    _attrs_181904940 = _loads('(dp1\n.')
-    _init_stream = _loads('cchameleon.core.generation\ninitialize_stream\np1\n.')
-    _attrs_181903724 = _loads('(dp1\n.')
-    _attrs_181905996 = _loads('(dp1\nVhref\np2\nV/\ns.')
-    _attrs_181905580 = _loads('(dp1\nVhref\np2\nV?row=${repeat.row.number}&col=${repeat.cell.number}\np3\ns.')
-    _init_default = _loads('cchameleon.core.generation\ninitialize_default\np1\n.')
     _re_amp = _loads("cre\n_compile\np1\n(S'&(?!([A-Za-z]+|#[0-9]+);)'\np2\nI0\ntRp3\n.")
-    _attrs_181904652 = _loads('(dp1\nVid\np2\nVboard\np3\ns.')
-    _attrs_181905164 = _loads('(dp1\n.')
+    _attrs_167426764 = _loads('(dp1\n.')
+    _attrs_167426316 = _loads('(dp1\n.')
+    _attrs_167427788 = _loads('(dp1\nVhref\np2\nV/\ns.')
+    _attrs_167424524 = _loads('(dp1\n.')
+    _attrs_167427052 = _loads('(dp1\n.')
+    _attrs_167425836 = _loads('(dp1\n.')
+    _init_stream = _loads('cchameleon.core.generation\ninitialize_stream\np1\n.')
+    _attrs_167426508 = _loads('(dp1\nVid\np2\nVboard\np3\ns.')
+    _attrs_167424300 = _loads('(dp1\n.')
+    _init_default = _loads('cchameleon.core.generation\ninitialize_default\np1\n.')
+    _attrs_167425068 = _loads('(dp1\n.')
+    _attrs_167426956 = _loads('(dp1\n.')
+    _attrs_167424812 = _loads('(dp1\n.')
+    _attrs_167425260 = _loads('(dp1\n.')
     _init_tal = _loads('cchameleon.core.generation\ninitialize_tal\np1\n.')
+    _attrs_167427372 = _loads('(dp1\nVhref\np2\nV?row=${repeat.row.number}&col=${repeat.cell.number}\np3\ns.')
     def render(econtext, rcontext=None):
         macros = econtext.get('macros')
         _translate = econtext.get('_translate')
@@ -35,19 +36,85 @@ def bind():
         u'None'
         _domain = None
         _write('<!DOCTYPE html>\n')
-        attrs = _attrs_181882732
+        attrs = _attrs_167424300
         _write(u'<html>\n  ')
-        attrs = _attrs_181903468
+        attrs = _attrs_167424524
         _write(u'<head>\n    ')
-        attrs = _attrs_181903724
+        attrs = _attrs_167424812
         _write(u'<title>Tic! Tac!  And even Toe!</title>\n    ')
-        attrs = _attrs_181903980
+        attrs = _attrs_167425068
         _write(u'<style>\n      #board {\n        border-collapse: collapse;\n      }\n      #board td {\n        width: 10em;\n        height: 10em;\n        border: 1px solid black;\n        text-align: center;\n      }\n      #board td a:hover {\n        background-color: #668800;\n      }\n      #board td a {\n        width: 2em;\n        height: 2em;\n        display: block;\n        font-size: 5em;\n        text-decoration: none;\n        line-height: 2em;\n      }\n    </style>\n  </head>\n  ')
-        attrs = _attrs_181904172
+        attrs = _attrs_167425260
+        u'winner'
         _write(u'<body>\n    ')
-        attrs = _attrs_181904428
-        _write(u'<p>Your move.</p>\n    ')
-        attrs = _attrs_181904652
+        _tmp1 = econtext['winner']
+        if _tmp1:
+            pass
+            _write(u'')
+            attrs = _attrs_167425836
+            u'winner'
+            _write(u'<p>')
+            _tmp1 = econtext['winner']
+            _tmp = _tmp1
+            if (_tmp.__class__ not in (str, unicode, int, float, )):
+                try:
+                    _tmp = _tmp.__html__
+                except:
+                    _tmp = _translate(_tmp, domain=_domain, mapping=None, target_language=target_language, default=None)
+                else:
+                    _tmp = _tmp()
+                    _write(_tmp)
+                    _tmp = None
+            if (_tmp is not None):
+                if not isinstance(_tmp, unicode):
+                    _tmp = str(_tmp)
+                if ('&' in _tmp):
+                    if (';' in _tmp):
+                        _tmp = _re_amp.sub('&amp;', _tmp)
+                    else:
+                        _tmp = _tmp.replace('&', '&amp;')
+                if ('<' in _tmp):
+                    _tmp = _tmp.replace('<', '&lt;')
+                if ('>' in _tmp):
+                    _tmp = _tmp.replace('>', '&gt;')
+                _write(_tmp)
+            _write(u' wins.</p>\n    ')
+        u'not winner'
+        _write(u'\n    ')
+        _tmp1 = not econtext['winner']
+        if _tmp1:
+            pass
+            _write(u'\n      ')
+            attrs = _attrs_167426316
+            u'player'
+            _write(u'<p>Your move ')
+            _tmp1 = econtext['player']
+            _tmp = _tmp1
+            if (_tmp.__class__ not in (str, unicode, int, float, )):
+                try:
+                    _tmp = _tmp.__html__
+                except:
+                    _tmp = _translate(_tmp, domain=_domain, mapping=None, target_language=target_language, default=None)
+                else:
+                    _tmp = _tmp()
+                    _write(_tmp)
+                    _tmp = None
+            if (_tmp is not None):
+                if not isinstance(_tmp, unicode):
+                    _tmp = str(_tmp)
+                if ('&' in _tmp):
+                    if (';' in _tmp):
+                        _tmp = _re_amp.sub('&amp;', _tmp)
+                    else:
+                        _tmp = _tmp.replace('&', '&amp;')
+                if ('<' in _tmp):
+                    _tmp = _tmp.replace('<', '&lt;')
+                if ('>' in _tmp):
+                    _tmp = _tmp.replace('>', '&gt;')
+                _write(_tmp)
+            _write(u'.</p>\n    ')
+        _write(u'\n    ')
+        attrs = _attrs_167426508
         u'board'
         _write(u'<table id="board">\n      ')
         _tmp1 = econtext['board']
@@ -55,7 +122,7 @@ def bind():
         (_tmp1, _tmp2, ) = repeat.insert('row', _tmp1)
         for row in _tmp1:
             _tmp2 = (_tmp2 - 1)
-            attrs = _attrs_181904940
+            attrs = _attrs_167426764
             u'row'
             _write(u'<tr>\n        ')
             _tmp3 = row
@@ -63,9 +130,9 @@ def bind():
             (_tmp3, _tmp4, ) = repeat.insert('cell', _tmp3)
             for cell in _tmp3:
                 _tmp4 = (_tmp4 - 1)
-                attrs = _attrs_181905260
+                attrs = _attrs_167427052
                 _write(u'<td>')
-                attrs = _attrs_181905580
+                attrs = _attrs_167427372
                 "join(u'?row=', value('repeat.row.number'), u'&col=', value('repeat.cell.number'))"
                 _write(u'<a')
                 _tmp5 = ('%s%s%s%s' % (u'?row=', _lookup_attr(repeat.row, 'number'), u'&col=', _lookup_attr(repeat.cell, 'number'), ))
@@ -124,9 +191,9 @@ def bind():
                 break
             _write(' ')
         _write(u'\n    </table>\n    ')
-        attrs = _attrs_181905164
+        attrs = _attrs_167426956
         _write(u'<p>')
-        attrs = _attrs_181905996
+        attrs = _attrs_167427788
         _write(u'<a href="/">New game</a></p>\n  </body>\n</html>')
         return _out.getvalue()
     return render
--- tictactoe/views.py
+++ tictactoe/views.py
@@ -1,4 +1,18 @@
 
+def get_winner(board):
+
+    diagonal1 = [board[i][i] for i in range(3)]
+    diagonal2 = [board[2-i][i] for i in range(3)]
+    for brd in [board, zip(*board), [diagonal1, diagonal2]]:
+        for row in brd:
+            r = ''.join(row)
+            if r == 'OOO':
+                return 'O'
+            elif r == 'XXX':
+                return 'X'
+    return None
+
+
 def my_view(request):
 
     if (not request.session.has_key('game')
@@ -9,7 +23,6 @@ def my_view(request):
                                    ['', '', '']]
 
     board = request.session['game']
-
     if request.params.get('row') and request.params.get('col'):
         row = int(request.params.get('row')) - 1
         col = int(request.params.get('col')) - 1
@@ -20,5 +33,9 @@ def my_view(request):
             else:
                 request.session['move'] = 'X'
 
-    return {'board': request.session['game'],}
+    winner = get_winner(board)
+
+    return {'board': request.session['game'],
+            'winner': winner,
+            'player': request.session['move']}
 
